e41f587d999507941d1de2fa542b4f037208f656
loukik4860 <loukikhajare16@gmail.com>
2024-03-28 12:10:04 +0530

    database settings changed

--- sessionAuthentication/sessionAuthentication/settings.py
+++ sessionAuthentication/sessionAuthentication/settings.py
@@ -67,9 +67,9 @@ WSGI_APPLICATION = "sessionAuthentication.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        "NAME": "sessionauth",
+        'USER': "root",
+        'PASSWORD': "root",
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
