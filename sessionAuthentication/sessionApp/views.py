9c03526e049e8eb73b2ce4cd91038d604dfa8bf3
loukik4860 <loukikhajare16@gmail.com>
2024-03-29 14:29:49 +0530

    push

--- sessionAuthentication/sessionApp/views.py
+++ sessionAuthentication/sessionApp/views.py
@@ -18,6 +18,15 @@ from .utils import send_activation_email
 
 # Create your views here.
 
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class getCSRFToken(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        return Response({'success': 'csrf Cookies set'})
+
+
+@method_decorator(csrf_protect, name='dispatch')
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
 
@@ -33,3 +42,33 @@ class RegistrationView(APIView):
             send_activation_email(user.email, activation_url)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class ActivateView(APIView):
+    permission_classes = [AllowAny]
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class Activation_Confirm(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        uid = request.data.get('uid')
+        token = request.data.get('token')
+
+        if not uid or not token:
+            return Response({'detail': 'missing uid or token'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            uid = force_str(urlsafe_base64_decode(uid))
+            user = User.objects.get(pk=uid)
+            if default_token_generator.check_token(user, token):
+                if user.is_active:
+                    return Response({'details': 'Account is aready Activated'}, status=status.HTTP_200_OK)
+                user.is_active = True
+                user.save()
+                return Response({'detail': 'Account activated Successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'details': 'Invalid Activation link'}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({'details': 'Invalid activation link'}, status=status.HTTP_400_BAD_REQUEST)
